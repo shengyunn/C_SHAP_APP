c0f144e11bccfb3859fa2f20794373298079eb10
聖允 陳 <ryan082795@gmail.com>
2023-12-17 19:30:01 +0800

    yum

---
+++ WpfApp1/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WpfApp1
+{
+    internal class Student
+    {
+        public String StudentId { get; set; }
+        public String StudentName { get; set; }
+        public override string ToString()
+        {
+            return $"{StudentId} {StudentName}";
+        }
+    }
+}
---
+++ WpfApp1/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace WpfApp1
+{
+    internal class Teacher
+    {
+        public String? TeacherName { get; set; }
+        public ObservableCollection<Course> TeachingCourses { get; set; } = new ObservableCollection<Course>();
+
+        public override string ToString()
+        {
+            return $"{TeacherName}";
+        }
+    }
+}
